55075dd5390710002c953ae378e84ed1814c8e1c
Nathan D Leathers <nathan.d.leathers@icloud.com>
2022-05-31 11:52:35 -0700

    work in progress

--- guessing_game.py
+++ guessing_game.py
@@ -1,1 +1,27 @@
+import random
+
 class GuessingGame():
+
+    def __init__(self,guess):
+        self.guess = input()
+
+    def solved(self):
+        print(f"Guessing game ")
+
+
+
+game = GuessingGame(random.randint(1,100))
+
+last_guess  = None
+last_result = None
+
+while game.solved() == False:
+  if last_guess != None: 
+    print(f"Oops! Your last guess ({last_guess}) was {last_result}.")
+    print("")
+
+  last_guess  = input("Enter your guess: ")
+  last_result = game.guess(last_guess)
+
+
+print(f"{last_guess} was correct!")
